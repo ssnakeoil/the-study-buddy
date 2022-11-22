f408c5a371857549e9042cd53bbd75ba6800f5f2
Ryan Villanueva <ryanueva@gmail.com>
2022-11-21 21:04:31 -0500

    edit js

--- assets/script.js
+++ assets/script.js
@@ -11,4 +11,16 @@
 // var nineBtn = document.querySelector('.nine-button');
 // var tenBtn = document.querySelector('.ten-button');
 
-
+// fetch('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley',{
+//      method: 'Post',
+//      headers: {
+//         'Content-Type': 'application/json'
+//      },
+//      body: JSON.stringify({
+//         name: 'User 1'
+//      })
+// }).then(res => {
+//     return res.json()
+// })
+// .then(data => console.log(data))
+// .catch(error => console.log('Error'))
